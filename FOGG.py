092d4f82c46dd9644451625550dc623a707838bb
Fwldom <111840687+fwldom@users.noreply.github.com>
2023-06-10 22:18:36 +0330

    F

--- FOGG.py
+++ FOGG.py
@@ -1,14 +1,16 @@
 import turtle
-num = int(turtle.textinput(title="Enter Number", prompt=("Enter forward !")))
-num2 = int(turtle.textinput(title="Enter Number", prompt=("Enter left to shekl !")))
-num3 = int(turtle.textinput(title="Enter Number", prompt=("Enter left har bar !  ")))
-width = int(turtle.textinput(title="Enter Number", prompt=("Enter width !  ")))
+num = int(turtle.textinput(title="Enter Number Forward", prompt=("Enter Forward !")))
+numr1 = int(turtle.textinput(title="Enter Number Range", prompt=("Enter Number Range Main !")))
+numr2 = int(turtle.textinput(title="Enter Number Range", prompt=("Enter Number on Range Main !")))
+num2 = int(turtle.textinput(title="Enter Number left", prompt=("Enter left Range !")))
+num3 = int(turtle.textinput(title="Enter Number Right", prompt=("Enter left End Range  !  ")))
+width = int(turtle.textinput(title="Enter Number width", prompt=("Enter width !  ")))
 laki = turtle.Turtle()
 laki.shape('turtle')
 laki.width(width)
-for i in range(50):
-    for j in range(8):
+for i in range(numr1):
+    for j in range(numr2):
         laki.forward(num)
         laki.left(num2)
-    laki.left(num3)
+    laki.right(num3)
 input()
